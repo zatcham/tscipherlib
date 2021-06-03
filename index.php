c0e91a9eed3e7cef950f3e01840c0d8384f4c709
Zach <37421850+zatcham@users.noreply.github.com>
2021-06-03 12:57:06 +0100

    Resolved a bug surrounding PHPs handling of unicode characters + also added the ancillary HTML code required to implement encrypting
    Encrypting functionalities as shown in the HTML is currently disabled whilst the PHP element of this is built. This functionality is likely to not be finished for a while, as the deciphering element was purely designed to be utilised alongside the ns-pCB Web Decrypter
    
    New function "unichr" assists with the way the decryption process works.

--- index.php
+++ index.php
@@ -1,16 +1,35 @@
+<!-- 
+    tscipherlib v4.1 - PHP Edition
+    Originally written by tomrow, converted to PHP by zatcham
+    May 2021
+-->
 <html>
+    <head>
+        <meta charset="windows-1252"/>
+    </head>
     <form enctype='multipart/form-data' action='' method='post'>
-        <label for="todecrypt">Enter encrypted string (without square brackets) : <span style="color: red;">*</span> </label>
-        <input type='text' id="todecrypt" name='todecrypt' required>
+
+    <input type="radio" id="rad1" name="select" value="Encrypt" disabled>
+    <label for="rad1">Encrypt</label><br>
+    <input type="radio" id="rad2" name="select" value="Decrypt">
+    <label for="rad2">Decrypt</label><br>
+        <h5> Encryption options: </h5>
+        <label for="toencrypt">Enter string to encrypt : </label>
+        <input type='text' id="toencrypt" name='toencrypt' disabled>
+
+        <label for="kettoencrypt">Enter key to encrypt with : </label>
+        <input type='text' id="kettoencrypt" name='kettoencrypt' disabled>
+
+        <h5> Decryption options: </h5>
+        <label for="todecrypt">Enter encrypted string (without square brackets) : </label>
+        <input type='text' id="todecrypt" name='todecrypt'>
                             
-        <label for="kettodecrypt">Enter key : <span style="color: red;">*</span> </label>
-        <input type='text' id="kettodecrypt" name='kettodecrypt' required>
+        <label for="kettodecrypt">Enter key to decrypt with: </label>
+        <input type='text' id="kettodecrypt" name='kettodecrypt'>
+        <br>
         <input type='submit' name='submit' value='Process' style="margin: 10px;">
     </form>
 </html>
-
-
-
 
 <?php
 
@@ -24,26 +43,52 @@
 //echo decipherTs("152, 210, 77, 136, 152, 211, 78, 136, 147, 188, 246, 113, 123, 188, 247, 113, 124, 183, 223, 26, 101, 159, 223, 27, 37, 159, 218, 3, 13, 135, 193, 3, 14, 72, 193, 252, 85, 143, 10, 68, 78, 143, 201, 67, 79, 118, 176, 43, 53, 111, 176, 234, 52, 111, 151, 209, 220, 86, 143, 208, 218, 84, 143, 183, 192, 251, 117, 175, 192, 249, 115, 174, 184, 127, 186, 52, 61, 126, 184, 50, 60, 118, 158, 216, 34, 92, 156, 214, 224, 91, 148, 188, 199, 64, 122, 187, 196, 254, 121, 178, 170, 229, 95, 152, 163, 227, 29, 151, 161, 42, 100, 221, 231, 35, 99, 156, 230, 33, 73, 132, 142, 7, 66, 130, 140, 7, 65, 105", 1);
 
 if (isset($_POST['submit'])) {
-    if (!empty($_POST['kettodecrypt'])) {
-        $k = ($_POST['kettodecrypt']);
-        $s = ($_POST['todecrypt']);
-        $x = decipherTs($s, $k);
-        echo ($x);
+    if (!empty($_POST['select'])) {
+        if ($_POST['select'] == "Encrypt") {
+            if (!empty($_POST['kettoencrypt'])) {
+                if (!empty($_POST['toencrypt'])) {
+                    $k = ($_POST['toencrypt']);
+                    $s = ($_POST['kettoencrypt']);
+                    $x = encodeTs($s, $k);
+                    // $y = implode("','", $x);
+                    print_r($x);
+                    echo ($x);
+                } else {
+                    echo ("Must enter string to encrypt!");
+                }
+            } else {
+                echo ("Must enter key to encrypt with!");
+            }
+        } elseif ($_POST['select'] == "Decrypt") {
+            if (!empty($_POST['kettodecrypt'])) {
+                if (!empty($_POST['todecrypt'])) {
+                    $k = ($_POST['kettodecrypt']);
+                    $s = ($_POST['todecrypt']);
+                    $x = decipherTs($s, $k);
+                    echo ($x);
+                } else {
+                    echo ("Must enter string to decrypt!");
+                }
+            } else {
+                echo ("Must enter key to decrypt with!");
+            }
+        } else {
+            echo ("Must select an option!");
+        }
     }
 }
 
 
 function encodeTs($in, $key) {
-    $o = [];
+    $o = "";
     for ($i = 0; $i < strlen($in); $i++) {
         $j = ($i + 1);
         $o .= (
-            (ord ($in[$i]) + scrambleTs ($j, $key)) % 255);
+            (ord ($in[$i]) + scrambleTs ($j, intval($key))) % 255);
         //echo ($i . "<br>");
     }
     return $o;
 }
-
 
 function decipherTs($array, $key) {
     $o = "";
@@ -54,7 +99,8 @@ function decipherTs($array, $key) {
         // $t = ($m % 255);
         // $a = (chr($m));
         //echo ($m . "<br>");
-        $o = ($o . chr(newmod($m, 255)));
+        //echo (newmod($m, 255));
+        $o = ($o . unichr(newmod($m, 255)));
     }
     return $o;
 }
@@ -91,6 +137,10 @@ function scrambleTs($iterate, $key) {
 
 function newmod($a, $b) {
     return ($a - $b * floor($a / $b));
+}
+
+function unichr($x) {
+    return mb_convert_encoding('&#' . intval($x) . ';', 'UTF-8', 'HTML-ENTITIES');
 }
 
 ?>
